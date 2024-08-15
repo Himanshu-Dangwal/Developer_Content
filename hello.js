0cddf399d6a0e70e540e8732f6d107640cbfa064
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-08-15 12:52:32 -0700

    Callbacks in JS

--- hello.js
+++ hello.js
@@ -1,1 +1,18 @@
 console.log("Hello");
+
+//Callback Functions::
+
+function calculcator(a,b,operation){
+    return operation(a,b);
+}
+
+function sum(a,b) {
+    return a+b;
+}
+
+function sub(a,b){
+    return a-b;
+}
+
+console.log(calculcator(10,12,sum));
+console.log(calculcator(10,12,sub));
