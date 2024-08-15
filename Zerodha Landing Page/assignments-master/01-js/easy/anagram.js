0ea06e10666b94a1af2a5bc67dfbb7661edc448a
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-08-14 18:26:45 -0700

    Anagram done, learnt Regex and Map

--- Zerodha Landing Page/assignments-master/01-js/easy/anagram.js
+++ Zerodha Landing Page/assignments-master/01-js/easy/anagram.js
@@ -5,6 +5,28 @@
 */
 
 function isAnagram(str1, str2) {
+    str1 = str1.replace(/\s+/g,'').toLowerCase();
+    str2 = str2.replace(/\s+/g,'').toLowerCase();
+
+    if(str1.length !== str2.length) return false;
+
+    const createMap = (s) => {
+      mp = {}
+      for(const char of s){
+        mp[char] = (mp[char] || 0) + 1;
+      }
+      return mp;
+    }
+
+    mp1 = createMap(str1);
+    mp2 = createMap(str2);
+
+    for(const char in mp1){
+      if(mp1[char] !== mp2[char]){
+        return false;
+      }
+    }
+    return true;
 
 }
 
