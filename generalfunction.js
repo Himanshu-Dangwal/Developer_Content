f83813939a9cc3c19a1592053974a0b27496e90d
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-08-15 13:44:13 -0700

    Some common string and array functions

---
+++ generalfunction.js
@@ -0,0 +1,22 @@
+let nam = "Himanshu"
+console.log(nam.length);
+
+let str = "Heelo World World"
+console.log(str.indexOf("World"))
+console.log(str.lastIndexOf("World"))
+
+let slicedString = nam.slice(1,4);
+console.log(slicedString)
+
+str = nam.replace("ima","pqr")
+console.log(nam)
+console.log(str)
+
+let sentence = "Hello my name is Himanshu"
+let words = sentence.split(' ');
+console.log(words)
+
+nam = "   Himanshu❤️Roju   "
+nam = nam.trim()
+console.log(nam)
+
