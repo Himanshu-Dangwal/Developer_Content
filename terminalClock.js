82239721bc2f47ec230ba0b186165fd6fd37878b
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-08-15 13:19:46 -0700

    Terminal Clock created

---
+++ terminalClock.js
@@ -0,0 +1,11 @@
+
+function clock(){
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    console.log(`${hours}:${minutes}:${seconds}`);
+}
+
+setInterval(clock,1000);
