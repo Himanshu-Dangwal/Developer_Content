84b58bf82d43165635cd60d53ec79be135e05000
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-08-17 13:46:05 -0700

    Learn using basic filter and reduce methods

--- map.js
+++ map.js
@@ -14,3 +14,19 @@ const values2 = arr.map((ele)=>{
 })
 
 console.log(values2)
+
+//Using filter to find the even values
+
+const evenValues = arr.filter((ele)=>{
+    return (ele%2 == 0);
+})
+
+console.log(evenValues)
+
+//Using reduce to find the sum of all elements of an array
+
+const sum = arr.reduce((acc,curr) => {
+    return acc += curr;
+},0)
+
+console.log(sum);
