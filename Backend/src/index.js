7eadc11e97e6f30fae3226ad8df7158b72270929
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-08-22 15:52:32 -0700

    Refactor backend , connecting to mongoDB

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,7 +1,24 @@
 const express = require("express")
+const mongoose = require("mongoose")
+
 const app = express();
+app.use(cors())
 
 const PORT = 8080;
+const URI = "mongodb://localhost:27017/"
+
+async function connectToMongo(){
+    mongoose.set("strictQuery",false);
+
+    try{
+        await mongoose.connect(URI)
+        console.log("Successfully connected to the database");
+    }catch (err){
+        console.log(`Error connecting to the database ${err.message}`)
+    }
+}
+
+connectToMongo().catch(err => console.log("Some internal error"))
 
 app.get("/",(req,res)=>{
     res.send("I'm an express server from a new branch");
