f70606ea1e3fb22ce82ca855446ec95f45041452
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-08-22 15:44:06 -0700

    Started To-Do

--- ReactDiff/@latest/src/App.jsx
+++ ReactDiff/@latest/src/App.jsx
@@ -3,41 +3,53 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-  const [todos,setTodos] = useState([{
-    title : "Study",
-    description : "Learn about yourself",
-    isCompleted : false
-  },{
-    title : "Make Friends",
-    description : "Have to search new friends",
-    isCompleted : false
+  const [todos, setTodos] = useState([{
+    title: "Study",
+    description: "Learn about yourself",
+    isCompleted: false
+  }, {
+    title: "Make Friends",
+    description: "Have to search new friends",
+    isCompleted: false
   }])
 
   return (
     <div>
-      
-        <Button count={count} setCount={setCount}></Button>
-        <Todo todos={todos} setTodos={setTodos}></Todo>
+      <Button count={count} setCount={setCount} />
+      <Todos todos={todos} setTodos={setTodos} />
     </div>
   )
 }
 
-//Button is a component that takes props (count and setCount to render itself and changes its appearance according to the state that it recieves)
-function Button(props){
-
-  function handleClick(){
-    props.setCount(props.count+1);
+// Button is a component that takes props (count and setCount to render itself and changes its appearance according to the state that it receives)
+function Button(props) {
+  function handleClick() {
+    props.setCount(props.count + 1);
   }
 
   return <button onClick={handleClick}>Count = {props.count}</button>
 }
 
-//To DOs
+// Todos component renders a list of Todo components
+function Todos(props) {
+  return (
+    <div>
+      {props.todos.map((todo, index) => (
+        <Todo key={index} todo={todo} />
+      ))}
+    </div>
+  )
+}
 
-function Todo(props){
-  return (<>
-    <h1>{props.todos}</h1>
-  </>)
+// Todo component receives todo as a prop
+function Todo({ todo }) {
+  return (
+    <>
+      <h1>{todo.title}</h1>
+      <h2>{todo.description}</h2>
+      <h3>{todo.isCompleted ? "Completed" : "Not Completed"}</h3>
+    </>
+  )
 }
 
 export default App
