8be2794e65f71b85610bf65795ba56d2980819c4
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-08-25 14:11:27 -0700

    Refactored a bit

--- Zerodha Landing Page/assignments-master/week-6/3-use-ref/src/components/Assignment2.jsx
+++ Zerodha Landing Page/assignments-master/week-6/3-use-ref/src/components/Assignment2.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState, useCallback, useRef } from 'react';
 
 // Create a component that tracks and displays the number of times it has been rendered. Use useRef to create a variable that persists across renders without causing additional renders when it changes.
@@ -18,4 +19,4 @@ export function Assignment2() {
             <button onClick={handleReRender}>Force Re-render</button>
         </div>
     );
-};
+}
