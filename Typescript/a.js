d3d18c7330e8c787be6b641b70959f6c57968bdc
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-09-01 10:39:37 -0700

    Checked any type, can be merged

--- Typescript/a.js
+++ Typescript/a.js
@@ -5,3 +5,9 @@ function sayName(firstName) {
     console.log(`My name is ${firstName}`);
 }
 sayName("Himanshu");
+// sayName(10);  This woun't work
+function sayMyname(firstName, lastName) {
+    console.log("My name is " + firstName + " " + lastName);
+}
+sayMyname(10, "Himanshu");
+sayMyname("Himanshu", "Dangwal");
