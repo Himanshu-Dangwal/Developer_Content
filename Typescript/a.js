406f39e260e594eb1ee9e0ad065d1b57c481392e
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-09-01 10:33:40 -0700

    Basic Types in Typescript

--- Typescript/a.js
+++ Typescript/a.js
@@ -1,2 +1,7 @@
-var x = 10;
+"use strict";
+const x = 10;
 console.log(x);
+function sayName(firstName) {
+    console.log(`My name is ${firstName}`);
+}
+sayName("Himanshu");
--- Typescript/a.ts
+++ Typescript/a.ts
@@ -1,2 +1,9 @@
 const x: number = 10;
 console.log(x);
+
+function sayName(firstName: string) {
+    console.log(`My name is ${firstName}`);
+}
+
+sayName("Himanshu");
+// sayName(10);  This woun't work
