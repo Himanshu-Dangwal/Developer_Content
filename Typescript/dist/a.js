d5c35c4a08a39eba8f9040e6b93074c2918493f7
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-09-02 16:16:21 -0700

    GEnerics in TS

--- Typescript/dist/a.js
+++ Typescript/dist/a.js
@@ -40,3 +40,19 @@ console.log(isLegalUser({
     lastName: "Dangwal",
     age: 17
 }));
+//Generics in JS
+function test(args) {
+    return args[0];
+}
+let op = test([1, 2, 3]);
+let op2 = test(["Himanshu", "Rojina"]);
+console.log(op);
+console.log(op2);
+//Understanding Generics
+function test2(args) {
+    return args[0];
+}
+let op3 = test2([1, 2, 3]);
+let op4 = test2(["Himanshu", "Rojina"]);
+console.log(op);
+console.log(op2);
--- Typescript/src/a.ts
+++ Typescript/src/a.ts
@@ -59,3 +59,28 @@ console.log(isLegalUser({
     lastName: "Dangwal",
     age: 17
 }))
+
+
+//Generics in JS
+function test(args: number[] | string[]) {
+    return args[0];
+}
+
+let op = test([1, 2, 3])
+let op2 = test(["Himanshu", "Rojina"])
+
+console.log(op)
+console.log(op2)
+
+
+//Understanding Generics
+
+function test2<T>(args: T[]): T {
+    return args[0];
+}
+
+let op3 = test2<number>([1, 2, 3])
+let op4 = test2<string>(["Himanshu", "Rojina"])
+
+console.log(op)
+console.log(op2)
