bd951478fdd84251f9978e1265a3647f1badf4aa
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-09-02 17:01:55 -0700

    Connected and created a table in NeonDB

--- Postgres/index.js
+++ Postgres/index.js
@@ -15,14 +15,35 @@ const pool = new Pool({
     },
 });
 
-async function getPgVersion() {
+// async function getPgVersion() {
+//     const client = await pool.connect();
+//     try {
+//         const result = await client.query('SELECT version()');
+//         console.log(result.rows[0]);
+//     } finally {
+//         client.release();
+//     }
+// }
+
+// getPgVersion();
+
+
+async function createTable() {
     const client = await pool.connect();
     try {
-        const result = await client.query('SELECT version()');
-        console.log(result.rows[0]);
+        const result = await client.query(`
+            CREATE TABLE "User" (
+                id SERIAL PRIMARY KEY,
+                username VARCHAR(20) UNIQUE NOT NULL
+            );
+        `);
+        console.log("Table 'User' created successfully.");
+    } catch (error) {
+        console.log(`Some error occurred while creating the table: ${error}`);
     } finally {
-        client.release();
+        client.release(); // Release the client back to the pool
     }
 }
 
-getPgVersion();
+createTable();
+
