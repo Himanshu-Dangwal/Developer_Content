cb19a6680afea90fd08324c6424a57fb49ca21db
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-12-24 14:38:49 -0800

    Simulation of a sample game with in memory store and a logger

--- StatefulBackends/dist/index.js
+++ StatefulBackends/dist/index.js
@@ -1,18 +1,16 @@
-import { games } from "./store"
-import { startLogger } from "./logger"
-
-
-startLogger();
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const store_1 = require("./store");
+const logger_1 = require("./logger");
+(0, logger_1.startLogger)();
 setInterval(() => {
-    games.push({
+    store_1.games.push({
         id: Math.random().toString(),
         whitePlayerName: "Himanshu",
         blackPlayerName: "Blacky",
         moves: []
-    })
-}, 5000)
-
+    });
+}, 5000);
 /*
 We need a sticky architecture to make sure the users are directed to the correct server and joined the correct room
 
@@ -22,4 +20,4 @@ Option 1 : We can have a routing service
 ?? Routing service is like a map, that stores which room is in which server.
 So whenever a user tries to connect to a room, the map redirects the user to the correct service.
 
-*/
+*/ 
