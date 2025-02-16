474e6d98fcf60e3869e1e8f12d26cbcc954145f2
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2025-02-16 09:43:06 -0800

    Currying using Bind and Closures

--- Practice/index.js
+++ Practice/index.js
@@ -21,3 +21,33 @@ let obj2 = {
 obj1.printName.call(obj2, "USA");
 
 //Call method (Function borrowing)
+
+
+
+//Bind Method
+let printFullNameBinded = obj1.printName.bind(obj2, "USA");
+console.log(printFullNameBinded)
+printFullNameBinded()
+
+
+
+//Curying in JS
+
+/* 1. Using bind method
+let multiply = function (x, y) { console.log(x * y); }
+
+let multiplyBy2 = multiply.bind(this, 2);
+multiplyBy2(3);
+
+*/
+
+// Using closures
+
+let multiply = function (x) {
+    return function (y) {
+        console.log(x * y);
+    }
+}
+
+let multiplyBy3 = multiply(3);
+multiplyBy3(4);
