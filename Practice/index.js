ca810479f9f2cc94187c8b5dedee7ef752585dc0
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2025-02-19 20:06:33 -0800

    Promises, call, apply, bind and polyfills

--- Practice/index.js
+++ Practice/index.js
@@ -1,57 +1,57 @@
-console.log("Hello");
+// console.log("Hello");
 
-let obj1 = {
-    firstName: "Himanshu",
-    lastName: "Dangwal",
-    printName: function (hometown) {
-        console.log(`Object is ${this.firstName} ${this.lastName} and lives in ${hometown}`)
-    }
-}
-// function printName(hometown) {
-//     console.log(`Object is ${this.firstName} ${this.lastName} and lives in ${hometown}`)
+// let obj1 = {
+//     firstName: "Himanshu",
+//     lastName: "Dangwal",
+//     printName: function (hometown) {
+//         console.log(`Object is ${this.firstName} ${this.lastName} and lives in ${hometown}`)
+//     }
+// }
+// // function printName(hometown) {
+// //     console.log(`Object is ${this.firstName} ${this.lastName} and lives in ${hometown}`)
+// // }
+
+// obj1.printName("Dehradun");
+
+// let obj2 = {
+//     firstName: "Amit",
+//     lastName: "Chauhan"
 // }
 
-obj1.printName("Dehradun");
+// obj1.printName.call(obj2, "USA");
 
-let obj2 = {
-    firstName: "Amit",
-    lastName: "Chauhan"
-}
-
-obj1.printName.call(obj2, "USA");
-
-//Call method (Function borrowing)
+// //Call method (Function borrowing)
 
 
 
-//Bind Method
-let printFullNameBinded = obj1.printName.bind(obj2, "USA");
-console.log(printFullNameBinded)
-printFullNameBinded()
+// //Bind Method
+// let printFullNameBinded = obj1.printName.bind(obj2, "USA");
+// console.log(printFullNameBinded)
+// printFullNameBinded()
 
 
 
-//Curying in JS
+// //Curying in JS
 
-/* 1. Using bind method
-let multiply = function (x, y) { console.log(x * y); }
+// /* 1. Using bind method
+// let multiply = function (x, y) { console.log(x * y); }
 
-let multiplyBy2 = multiply.bind(this, 2);
-multiplyBy2(3);
+// let multiplyBy2 = multiply.bind(this, 2);
+// multiplyBy2(3);
 
-*/
+// */
 
-Function.prototype.myBind = function (...args) {
-    let functionCallingMyBind = this, boundThis = args[0], params = args.slice(1);
-    return function (...newArgs) {
-        return functionCallingMyBind.apply(boundThis, [...params, ...newArgs]);
-    };
-};
+// Function.prototype.myBind = function (...args) {
+//     let functionCallingMyBind = this, boundThis = args[0], params = args.slice(1);
+//     return function (...newArgs) {
+//         return functionCallingMyBind.apply(boundThis, [...params, ...newArgs]);
+//     };
+// };
 
-let multiply = function (x, y) { console.log(x * y); };
+// let multiply = function (x, y) { console.log(x * y); };
 
-let multiplyBy2 = multiply.myBind(this, 2);
-multiplyBy2(5);
+// let multiplyBy2 = multiply.myBind(this, 2);
+// multiplyBy2(5);
 
 
 
@@ -98,6 +98,7 @@ const API_BASE_URL = "https://www.algoexpert.io/api/fe/questions";
 async function fetchQuestions() {
     const response = await fetch(API_BASE_URL);
     const questions = await response.json();
+    console.log(questions);
 
     return questions;
 }
@@ -106,10 +107,11 @@ function getQuestionsByCategory(questions) {
     const questionsByCategory = {};
 
     questions.forEach((question) => {
-        if (questionsByCategory.hasOwnProperty(question[category])) {
-            questionsByCategory[quesion[category]].push(question);
+        console.log(question)
+        if (questionsByCategory.hasOwnProperty(question.category)) {
+            questionsByCategory[question.category].push(question);
         } else {
-            questionsByCategory[quesion[category]] = [question];
+            questionsByCategory[question.category] = [question];
         }
     });
 
@@ -125,7 +127,7 @@ async function printValues() {
     )) {
         console.log(category);
         questionsArray.forEach((question) => {
-            console.log(question.title);
+            console.log(question.name);
         });
     }
 }
